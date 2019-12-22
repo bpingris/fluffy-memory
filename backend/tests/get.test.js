e34cbcf0a3ec886bea3de9e28da4ab5506dca3df
gounaff <43850815+Gounaff@users.noreply.github.com>
2019-12-22 17:37:07 +0800

    Ajout test methode get

--- backend/tests/get.test.js
+++ backend/tests/get.test.js
@@ -1,5 +1,28 @@
-const blog = require('../src/routes/blog.js');
+const app = require("../src/app");
+const supertest = require("supertest");
+const request = supertest(app);
 
-test('Get with id that not exists', () => {
-	expect(blog.get('-12')).toBe("This article does not exist");
+const body = {
+	title: "test",
+	author: "testman",
+	content: "this is a test"
+}
+
+describe("Get method", () => {
+  it("Responds to a get with wrong id", async done => {
+    const res = await request.get("/blog/12");
+
+    expect(res.text).toBe("This article does not exist");
+    expect(res.status).toBe(404);
+    done();
+  });
+
+  it("Responds to a get with good id", async done => {
+  	const post = await request.post("/blog").send(body);
+
+  	const res = await request.get("/blog/" + post.body._id);
+
+  	expect(res.body).toStrictEqual(post.body);
+  	expect(res.status).toBe(200);
+  });
 });
