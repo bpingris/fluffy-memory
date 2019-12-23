b9f07502832e67ff97924735219338bd91f1f39c
Benoit Pingris <benoit.pingris@epitech.eu>
2019-12-23 21:57:18 +0800

    remove logging if test env

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,18 @@ const app = express();
 
 const blog = require("./routes/blog");
 
-app.use(morgan("tiny"));
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("tiny"));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
